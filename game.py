b6ddca4b6ce29d328aae30c6fd0534953d2bfc42
SenorPie <kaihanhakimi5@gmail.com>
2022-07-01 01:56:16 +0200

    Basic movement is done

--- game.py
+++ game.py
@@ -1,17 +1,29 @@
 import sys, pygame
+from player import Player
 from pygame import Color
 
 
 def game():
     pygame.init()
+    clock = pygame.time.Clock()
 
-    width, height = 400, 500
+    width, height = 600, 400
     screen = pygame.display.set_mode((width, height))
+    player = Player(position=(100, 100), size=(10, 10), color=Color(255, 0, 0), velocity=10)
+
     while True:
+        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
 
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_DOWN]:
+                    player.move_down()
+
+        player.draw_player(game_window=screen)
         pygame.display.flip()
+        clock.tick(60)
 
 if __name__ == "__main__":
     game()
--- player.py
+++ player.py
@@ -2,13 +2,19 @@ from turtle import position
 import pygame
 
 class Player:
-    def __init__(self, position: tuple, size: tuple, color: pygame.Color):
-        self.position = position
+    def __init__(self, position: tuple, size: tuple, color: pygame.Color, velocity):
+        self.x = position[0]
+        self.y = position[1]
         self.size = size
         self.color = color
-    
-    def draw_player(self, game_window):
-        player_rect = pygame.Rect(self.position[0], self.position[1],
-                                  self.size[0], self.size[1])
+        self.velocity = velocity
 
-        pygame.draw.rect(surface=game_window, color=self.color, rect=player_rect)
+        self.player_rect = pygame.Rect(position[0], position[1],
+                                       size[0], size[1])
+   
+    def draw_player(self, game_window):
+        pygame.draw.rect(surface=game_window, color=self.color, rect=self.player_rect)
+    
+    def move_down(self):
+        self.player_rect.move_ip(0, self.velocity)
+        
